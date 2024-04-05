5a83dd176860a1444b91daf3ee6b3e42cdc9a16a
Soumyadeep Ghosh <139796191+SoumyadeepGhosh16@users.noreply.github.com>
2024-04-05 21:37:07 +0530

    Add files via upload

---
+++ content/simpleInterest.java
@@ -0,0 +1,10 @@
+public class simpleInterest {
+    public static void main(String[] args) {
+        
+        float P=1, R=1,T=1;
+
+        float SI= (P*R*T)/100;
+
+        System.out.println("simple interest=" +SI);
+    }
+}
